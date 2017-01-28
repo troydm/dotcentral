0fac284b688a749c0cbc3e477389a0d92a8f38aa
troydm <d.geurkov@gmail.com>
2017-01-29 00:45:01 +0400

    added server configuration

--- configurations/common/configuration.rb
+++ configurations/common/configuration.rb
@@ -2,19 +2,21 @@
 git 'https://github.com/troydm/sshc.git', 'sshc'
 symlink 'bin/sshc', 'sshc/sshc'
 
-# install Powerline fonts
-git 'https://github.com/powerline/fonts.git', 'fonts'
+# install Powerline fonts on OSX and Linux only
 if os == 'linux'
     install_fontsdir = '~/.fonts'
 elsif os == 'osx'
     install_fontsdir = '~/Library/Fonts'
 end
-mkdir install_fontsdir
-ls('fonts',{:file => false}).each { |fontdir|
-    ls("fonts/#{fontdir}",{:grep => '.[ot]tf'}).each { |font|
-        symlink "#{install_fontsdir}/#{font}", "fonts/#{fontdir}/#{font}"
-    }
-}
+if install_fontsdir
+  git 'https://github.com/powerline/fonts.git', 'fonts'
+  mkdir install_fontsdir
+  ls('fonts',{:file => false}).each { |fontdir|
+      ls("fonts/#{fontdir}",{:grep => '.[ot]tf'}).each { |font|
+          symlink "#{install_fontsdir}/#{font}", "fonts/#{fontdir}/#{font}"
+      }
+  }
+end
 
 
 symlink '~/.Xresources', 'Xresources'
--- configurations/server/configuration.rb
+++ configurations/server/configuration.rb
@@ -1,4 +1,2 @@
 erb 'tmux.conf.erb'
 symlink '~/.tmux.conf', 'tmux.conf'
-erb 'bashrc.erb'
-source '~/.bashrc', 'bashrc'
