f37d39d9ab079a33c9a56875beedd49860d71daa
troydm <d.geurkov@gmail.com>
2017-11-23 00:29:05 +0200

    qutebrowser configuration added

---
+++ configurations/common/qutebrowser/config.py
@@ -0,0 +1,19 @@
+c.fonts.monospace='DejaVu Sans Mono'
+c.fonts.statusbar='12pt monospace'
+c.fonts.completion.category='bold 12pt monospace'
+c.fonts.completion.entry='12pt monospace'
+c.fonts.debug_console='12pt monospace'
+c.fonts.downloads='12pt monospace'
+c.fonts.hints='bold 12pt monospace'
+c.fonts.keyhint='12pt monospace'
+c.fonts.messages.error='12pt monospace'
+c.fonts.messages.info='12pt monospace'
+c.fonts.messages.warning='12pt monospace'
+c.fonts.prompts='12pt monospace'
+c.fonts.tabs='12pt monospace'
+
+config.bind('<Ctrl-i>','back')
+config.bind('<Ctrl-o>','forward')
+config.bind('<Ctrl-n>','tab-next')
+config.bind('<Ctrl-p>','tab-prev')
+config.bind('t','set-cmd-text -s :open -t ')
