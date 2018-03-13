0bb3cfc98b718a2d794d3711d041c8f211918253
troydm <d.geurkov@gmail.com>
2018-03-14 00:22:47 +0200

    hyper font size increased

--- configurations/common/hyper.js
+++ configurations/common/hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily: '"DejaVu Sans Mono for Powerline"',
