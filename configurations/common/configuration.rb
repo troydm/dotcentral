44f7675c6f1c9d02a0f9f8b97cb02a266d5dbca4
troydm <d.geurkov@gmail.com>
2019-05-18 23:50:04 +0300

    updated for central v0.2.4

--- configurations/common/configuration.rb
+++ configurations/common/configuration.rb
@@ -10,7 +10,7 @@ symlink 'bin/exp', 'exp/exp'
 git 'https://github.com/pepa65/tldr-bash-client.git', 'tldr'
 symlink 'bin/tldr', 'tldr/tldr'
 
-# install Powerline fonts on OSX and Linux only
+# install Nerd Fonts on OSX and Linux only
 if linux?
     if hostname == 'troymac'
       install_fontsdir = '~/.local/share/fonts'
@@ -21,30 +21,29 @@ elsif osx?
     install_fontsdir = '~/Library/Fonts'
 end
 if install_fontsdir
-  git 'https://github.com/powerline/fonts.git', 'fonts'
+  mkdir 'fonts'
+  curl 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf', 'fonts/DejaVu_Sans_Mono_Nerd_Font_Complete_Mono.ttf'
   mkdir install_fontsdir
-  ls('fonts',{:file => false}).each { |fontdir|
-      ls("fonts/#{fontdir}",{:grep => '.[ot]tf'}).each { |font|
-          if linux?
-            symlink "#{install_fontsdir}/#{font}", "fonts/#{fontdir}/#{font}"
-          elsif osx?
-            copy "fonts/#{fontdir}/#{font}", "#{install_fontsdir}/#{font}"
-          end
-      }
+  ls("fonts",grep: '.[ot]tf').each { |font|
+    if linux?
+      symlink "#{install_fontsdir}/#{font}", "fonts/#{font}"
+    elsif osx?
+      copy "fonts/#{font}", "#{install_fontsdir}/#{font}"
+    end
   }
 end
 
 # powerline command
 unless file_exists? 'bin/powerline'
   if linux? and hostname == "troymac"
-    shell "nix-shell -p stdenv --pure --command 'cd bin && gcc -o ./powerline -O2 ./powerline.c && strip ./powerline'", {:verbose => true, :silent => false}
+    shell "nix-shell -p stdenv --pure --command 'cd bin && gcc -o ./powerline -O2 ./powerline.c && strip ./powerline'", verbose: true, silent: false
   else
-    shell 'cd bin && gcc -o ./powerline -O2 ./powerline.c && strip ./powerline', {:verbose => true, :silent => false}
+    shell 'cd bin && gcc -o ./powerline -O2 ./powerline.c && strip ./powerline', verbose: true, silent: false
   end
 end
 
 # install ack
-curl 'https://beyondgrep.com/ack-2.22-single-file', 'bin/ack'
+curl 'https://beyondgrep.com/ack-2.28-single-file', 'bin/ack'
 chmod 'bin/ack', '0755'
 
 if linux?
@@ -63,6 +62,7 @@ if file_exists? '~/.config/fish/config.fish'
   erb 'fishrc.erb'
   source '~/.config/fish/config.fish', 'fishrc'
 end
+mkdir '~/.config/kitty'
 symlink '~/.config/kitty/kitty.conf', 'kitty.conf'
 symlink '~/.gitconfig', 'gitconfig'
 symlink '~/.gitignore', 'gitignore'
--- configurations/troypc/configuration.rb
+++ configurations/troypc/configuration.rb
@@ -1,18 +1,18 @@
 if linux?
-	symlink '~/.config/compton.conf', 'compton.conf'
-	symlink '~/.wallpapers', 'wallpapers'
-	symlink '~/.config/polybar', 'polybar'
-	symlink '~/.local/share/konsole/zenburn.colorscheme', '../common/zenburn.colorscheme'
-
-	# install FontAwesome
-	install_fontsdir = '~/.fonts'
-	git 'https://github.com/FortAwesome/Font-Awesome.git', 'fontawesome','v4.7.0'
-	mkdir install_fontsdir
-	ls('fontawesome',{:file => false}).each { |fontdir|
-	  ls("fontawesome/#{fontdir}",{:grep => '.[ot]tf'}).each { |font|
-	    symlink "#{install_fontsdir}/#{font}", "fontawesome/#{fontdir}/#{font}"
-	  }
-	}
-
-	run 'xmonad/configuration.rb'
+  symlink '~/.config/compton.conf', 'compton.conf'
+  symlink '~/.wallpapers', 'wallpapers'
+  symlink '~/.config/polybar', 'polybar'
+  symlink '~/.local/share/konsole/zenburn.colorscheme', '../common/zenburn.colorscheme'
+  
+  # install FontAwesome
+  install_fontsdir = '~/.fonts'
+  git 'https://github.com/FortAwesome/Font-Awesome.git', 'fontawesome','v4.7.0'
+  mkdir install_fontsdir
+  ls('fontawesome',file: false).each { |fontdir|
+    ls("fontawesome/#{fontdir}",grep: '.[ot]tf').each { |font|
+      symlink "#{install_fontsdir}/#{font}", "fontawesome/#{fontdir}/#{font}"
+    }
+  }
+  
+  run 'xmonad/configuration.rb'
 end
