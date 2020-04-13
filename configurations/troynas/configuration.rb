41c41baa3f448cb3cb6397465f625d93bc44b1e3
troydm <d.geurkov@gmail.com>
2020-04-13 14:48:06 +0300

    troynas arc_summary added, vimrc airline zoomwintab settings added

--- configurations/troynas/configuration.rb
+++ configurations/troynas/configuration.rb
@@ -6,3 +6,8 @@ source '~/.bashrc', 'bashrc'
 
 git 'https://github.com/troydm/tsundoku.git', 'tsundoku'
 symlink 'bin/tsundoku', 'tsundoku/tsundoku'
+
+unless file_exists? 'bin/arc_summary'
+  curl 'https://raw.githubusercontent.com/openzfs/zfs/384328e544b1847236a07df231e1b7b10e4cc6ce/cmd/arc_summary/arc_summary.py', 'bin/arc_summary'
+  chmod 'bin/arc_summary', '0755'
+end
