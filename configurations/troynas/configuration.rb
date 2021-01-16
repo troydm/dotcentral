a816506aa292638f96e0378c05e691ccf8863d41
troydm <d.geurkov@gmail.com>
2021-01-16 14:52:36 +0200

    x11vnc config added

--- configurations/troynas/configuration.rb
+++ configurations/troynas/configuration.rb
@@ -3,6 +3,7 @@ symlink '~/.tmux.conf', 'tmux.conf'
 symlink '~/.local/share/konsole/zenburn.colorscheme', '../common/zenburn.colorscheme'
 erb 'bashrc.erb'
 source '~/.bashrc', 'bashrc'
+symlink '~/.x11vncrc', 'x11vncrc'
 
 git 'https://github.com/troydm/tsundoku.git', 'tsundoku'
 symlink 'bin/tsundoku', 'tsundoku/tsundoku'
