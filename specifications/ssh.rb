60b212961e9a4e789240275b1e35065728c9777e
troydm <d.geurkov@gmail.com>
2021-10-04 14:39:28 +0300

    troypc linux xmonad configuration updated

--- specifications/ssh.rb
+++ specifications/ssh.rb
@@ -3,9 +3,9 @@ class SSH
   def self.configure(dir)
     ls(dir).each do |file|
       if file == 'config' || file_suffix(file) == '.pub'
-        chmod file, '0644'
+        chmod "#{dir}/#{file}", '0644'
       else
-        chmod file, '0600'
+        chmod "#{dir}/#{file}", '0600'
       end
       symlink "~/.ssh/#{file}", "#{dir}/#{file}"
     end
