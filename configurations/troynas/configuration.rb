58f6c9c5bca50c8f638618ff042fc0863edda09d
troydm <d.geurkov@gmail.com>
2022-02-18 15:23:46 +0200

    minor tsundoku related change

--- configurations/troynas/configuration.rb
+++ configurations/troynas/configuration.rb
@@ -6,7 +6,6 @@ source '~/.bashrc', 'bashrc'
 symlink '~/.x11vncrc', 'x11vncrc'
 
 git 'https://github.com/troydm/tsundoku.git', 'tsundoku'
-symlink 'bin/tsundoku', 'tsundoku/tsundoku'
 
 unless file_exists? 'bin/arc_summary'
   curl 'https://raw.githubusercontent.com/openzfs/zfs/384328e544b1847236a07df231e1b7b10e4cc6ce/cmd/arc_summary/arc_summary.py', 'bin/arc_summary'
